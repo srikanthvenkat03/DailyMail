646ad96879f3eaf0e145d59c31548d0564109ed5
Srikanth Venkatesan <149942762+srikanthvenkat03@users.noreply.github.com>
2025-09-07 20:15:33 +0530

    Delete checkgpu.py

--- checkgpu.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available!")
-    print("GPU Name:", torch.cuda.get_device_name(0))
-    print("Number of GPUs:", torch.cuda.device_count())
